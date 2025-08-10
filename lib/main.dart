93984d2c9a0308141a52eea40dbb784dddf0e19c
Owl Visions <63959950+owlvisions@users.noreply.github.com>
2025-08-11 01:27:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // تهيئة OneSignal مع App ID بتاعك
+  OneSignal.shared.setAppId('b188bee9-c63f-40fa-91b1-ebbe5b5ed43c');
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static const String oneSignalAppId = 'b188bee9-c63f-40fa-91b1-ebbe5b5ed43c'; // Change this
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WebViewContainer(),
+      title: 'From Hurghada',
       debugShowCheckedModeBanner: false,
+      home: WebViewContainer(),
     );
   }
 }
@@ -27,12 +31,29 @@ class _WebViewContainerState extends State<WebViewContainer> {
   final String url = 'https://fromhurghada.com';
 
   @override
+  void initState() {
+    super.initState();
+
+    // إعداد مستمع لفتح الإشعارات لو حبيت تعالج فتح الإشعارات
+    OneSignal.shared.setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // تقدر هنا تضيف كود خاص عند فتح إشعار
+      print('Notification opened: ${result.notification.jsonRepresentation()}');
+    });
+
+    // تفعيل طلب صلاحيات الإشعارات للمستخدم لو لازمة (خاصة على iOS)
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      print("Accepted permission: $accepted");
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebView(
         initialUrl: url,
         javascriptMode: JavascriptMode.unrestricted,
         navigationDelegate: (NavigationRequest request) {
+          // كل الروابط تفتح داخل التطبيق نفسه
           return NavigationDecision.navigate;
         },
       ),
